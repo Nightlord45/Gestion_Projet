e67f6c5633abef0862dd3b55e5f81e7312ad420d
Gatzby <jean-francois.carpentier@etu.univ-orleans.fr>
2014-02-13 10:10:20 +0100

    ajout des chaines de caractères pour tests

--- Mastermind/app/models/Decimal.java
+++ Mastermind/app/models/Decimal.java
@@ -8,6 +8,8 @@ public class Decimal{
     @Required
     private int code-secret[]  = new int[]{1,4,0,2}; 
     private int valeur[] = new int[]{1,3,1,0}; 
+     private int valeur1[] = new int[]{2,3,1,0}; 
+      private int valeur2[] = new int[]{1,4,0,2}; 
 
 
     public Decimal(int d){
@@ -15,7 +17,7 @@ public class Decimal{
 	}
 	
 	public compare(){
-		//String s1[] = new String[]{"A","B","C","D","E"};
+		/*//String s1[] = new String[]{"A","B","C","D","E"};
 		//String s2[] = new String[]{"X","Y","B","Z"};
 		List<String> myListe = new ArrayList<String>();
 		boolean trouve;
@@ -34,7 +36,7 @@ public class Decimal{
 		myListe.add(code-secret[i]);
 	}
 }
-	}
+	}*/
 	
 		
 	
